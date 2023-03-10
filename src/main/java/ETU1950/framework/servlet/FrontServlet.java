ad5a12ca5f049ec9f66b19740f96749f8821d25a
priscafehiarisoa <mytyrealy@gmail.com>
2023-03-10 10:59:30 +0300

    sprint2

--- src/main/java/ETU1950/framework/servlet/FrontServlet.java
+++ src/main/java/ETU1950/framework/servlet/FrontServlet.java
@@ -1,4 +1,4 @@
-package servlet;
+package ETU1950.framework.servlet;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -6,9 +6,11 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 //@WebServlet(name = "FrontServlet", value = "/FrontServlet")
 public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> MappingUrls;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request,response);
